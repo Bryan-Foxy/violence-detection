3c3c51ecc622846f5395fc2af79bae8e114f41af
Bryan-Foxy <armandbryan572@icloud.com>
2024-04-07 11:12:00 +0200

    requirement update

--- bash_require.sh
+++ bash_require.sh
@@ -1,3 +1,4 @@
 echo "tensorflow=="$(python -c 'import tensorflow as tf; print(tf.__version__)') > requirements.txt
 echo "tqdm=="$(python -c 'import tqdm; print(tqdm.__version__)') >> requirements.txt
 echo "tensorboard=="$(python -c 'import tensorboard; print(tensorboard.__version__)') >> requirements.txt
+echo "opencv-python=="$(python -c 'import cv2; print(cv2.__version__)') >> requirements.txt
